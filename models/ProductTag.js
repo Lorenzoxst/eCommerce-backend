737d38e43c4fe9737a4ddea621eefe681af3eb8d
renzoxst <lorenzoxst@gmail.com>
2021-09-12 16:24:16 -0500

    Completed models and routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.Integer,
+      reference: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.Integer,
+      reference: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
